917749148094876815649f820edc38e77a9f50ec
unknown <rajeev.sahu85@gmail.com>
2021-11-16 21:51:02 +0530

    finish

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -10,8 +10,7 @@ export default function UserList() {
             <Card/>
             {/* ok */}
             <Card/>
-            {/* ok */}
-            {/* ok */}
+            
         </div>
     )
 }
