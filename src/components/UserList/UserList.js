095ab25d0a06ba746aed97c1fa28167fc2ceb3dc
unknown <rajeev.sahu85@gmail.com>
2021-11-19 22:06:15 +0530

    finish

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -6,7 +6,7 @@ import styles from './UserList.module.css'
 export default function UserList() {
     return (
         <div className={styles.UserList} >
-            {/* ok */}
+
             <Card/>
             
             <Card/>
